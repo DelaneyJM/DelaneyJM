fe6b3bebe4216b34243fd3032bffb1620dc5701e
delaneyjm <jmikedelaney@gmail.com>
2022-04-26 23:46:46 -0300

    Added new code for processing PROD deploys involving AD groups

--- New-GOPDKTPApplicationDeployment.ps1
+++ New-GOPDKTPApplicationDeployment.ps1
@@ -543,129 +543,235 @@ Function New-GOPDKTPApplicationDeployment {
             ) -OutVariable errorRecord > $null
             throw $errorRecord
         }
+
+        # Check if we have a session already created to SCCM Primary or are
+        # already running this on the SCCM primary for the respective client(s).
+        try {
+            $pri = @{
+                PWGSC = "VAPW-CM1"
+                CSPS  = "PKEW-NACM1"
+                INFRA = "INFC-WV-CM1"
+            }
+            $cs = Get-CimSession -Name SCCM -ErrorAction Stop
+        } catch {
+            if($env:COMPUTERNAME -notin $pri.GetEnumerator().Value) {
+                $ToSession = $pri.GetEnumerator() |
+                    Where-Object { $env:USERDNSDOMAIN -match $_.name } |
+                        Select-Object -ExpandProperty Value
+                $SessionParams = @{
+                    Name = "SCCM"
+                    ComputerName = $ToSession
+                    SessionOption = New-CimSessionOption -Protocol Dcom
+                }
+                $cs = New-CimSession @SessionParams
+            }
+        } finally {
+            # We know what environment we are in.  Set the base namespace
+            $nsi = $pri.GetEnumerator() |
+                Where-Object { $env:USERDNSDOMAIN -match $_.name } |
+                    Select-Object -ExpandProperty Name
+            switch($nsi) {
+                "PWGSC" { $ns = "root\sms\site_cm1"; }
+                "CSPS"  { $ns = "root\sms\site_na1"; }
+                "INFRA" { $ns = "root\sms\site_in1"; }
+            }
+
+            # We need know if we are running this on a primary because
+            # otherwise, Get-CimInstance will fail to connect properly.
+            if(-not $cs) {
+                $OnPrimary = $true
+            } else {
+                $OnPrimary = $false
+            }
+        }
     }
     PROCESS {
-        #$desc = "Created by $engineer, $(Get-Date -format "dd\/MM\/yyyy")"
+        $desc = "Created by $engineer, $(Get-Date -format "dd\/MM\/yyyy")"
 
         # start with the ActiveDirectory portion
         if($coredomain) {
             # We're running this inside PSPC/SSC domain
-            $oupath = "DC=ad,DC=pwgsc-tpsgc,DC=gc,DC=ca"
-            $oupath = $oupath.Insert(0,"OU=SSC-SPC Administration,")
-            $oupath = $oupath.Insert(0,"OU=Resource Groups,OU=GOPDKTP,")
-            $oupath = $oupath.Insert(0,"$($Environment) Application Groups,")
-            $oupath = $oupath.Insert(0,"OU=$($vendor),OU=$($Department) ")
+            $oul = "DC=ad,DC=pwgsc-tpsgc,DC=gc,DC=ca"
+            $oul = $oul.Insert(0,"OU=GOPDKTP,OU=SSC-SPC Administration,")
+            $oul = $oul.Insert(0,"Application Groups,OU=Resource Groups,")
+            $oul = $oul.Insert(0,"OU=$($Department) $($Environment) ")
+            $oul = $oul.Insert(0,"OU=$($vendor),")
         } else {
             if($env:USERDNSDOMAIN -match "csps") {
-                $oupath = "DC=csps-efpc,DC=com"
+                $oul = "DC=csps-efpc,DC=com"
+                $oul = $oul.Insert(0,"OU=GOPDKTP,OU=SSC-SPC Administration,")
+                $oul = $oul.Insert(0,"Application Groups,OU=Resource Groups,")
+                $oul = $oul.Insert(0,"OU=$($Department) $($Environment) ")
+                $oul = $oul.Insert(0,"OU=$($vendor),")
             }
             if($env:USERDNSDOMAIN -match "infr") {
-                $oupath = "DC=ad,DC=infrastructure,DC=gc,DC=ca"
-                # OU=INFC RATE Application Groups,OU=Resource Groups,OU=Groups,OU=GOPDKTP,OU=SSC-SPC Administration,DC=ad,DC=infrastructure,DC=gc,DC=ca
-                # OU=INFC Production Application Groups,OU=Resource Groups,OU=Groups,OU=GOPDKTP,OU=SSC-SPC Administration,DC=ad,DC=infrastructure,DC=gc,DC=ca
+                $oul = "DC=ad,DC=infrastructure,DC=gc,DC=ca"
+                $oul = $oul.Insert(0,"OU=GOPDKTP,OU=SSC-SPC Administration,")
+                $oul = $oul.Insert(0,"OU=Resource Groups,OU=Groups,")
+                $oul = $oul.Insert(0,"$($Environment) Application Groups,")
+                $oul = $oul.Insert(0,"OU=$($vendor),OU=$($Department) ")
             }
         }
 
         # We've built our base search location.  Check that the OU exists in AD
         $OUParams = @{
             LDAPFilter = "(name=$($vendor))"
-            SearchBase = $oupath.Split(',',2)[-1]
+            SearchBase = $oul.Split(',',2)[-1]
             SearchScope = "Subtree"
         }
-        $ou = Get-ADOrganizationalUnit @OUParams
+        $EnvPath = $oul.Split(',',3)[1].Remove(0,3)
+        $ou = Get-ADOrganizationalUnit @OUParams -Properties canonicalname
         if(-not $ou) {
-            # Didn't find the OU. Create it.
-            $vbm = "[GOPDKTP-SCCM] { PROCESS }: ActiveDirectory OU does"
-            $vbm += " not exist for $($vendor) at "
-            $vbm += "$($oupath.Split(",",2)[-1])."
+            # Didn't find the OU. Send a notification.
+            $vbm.Append("[GOPDKTP-SCCM] { PROCESS }: AD OU does not ")> $null
+            $vbm.Append("exist for $($vendor) under $($EnvPath)") > $null
             Write-Verbose -Message $vbm.ToString() -Verbose
             $vbm.Clear() > $null
 
-            $vbm = "[GOPDKTP-SCCM] { PROCESS }: This will be created in"
-            $vbm += " order to process the deployment"
+            $vbm.Append("[GOPDKTP-SCCM] { PROCESS }: A new OU will be ") > $null
+            $vbm.Append("created in order to process the deployment") > $null
             Write-Verbose -Message $vbm.ToString() -Verbose
+            $vbm.Clear() > $null
+
+            # Setup the creation parameters and create the new OU
             $NewOUParams = @{
-                Name = $oupath.Split(',',2)[0]
-                Path = $oupath.Split(',',2)[-1]
+                Name = $oul.Split(',',2)[0].Remove(0,3)
+                Path = $oul.Split(',',2)[-1]
                 ProtectedFromAccidentalDeletion = $false
+                OutVariable = "ou"
+                ErrorAction = "Stop"
             }
-            New-ADOrganizationalUnit @NewOUParams > $null
+            try {
+                New-ADOrganizationalUnit @NewOUParams > $null
+            } catch {
+                # An error occurred when creating the new OU. We're going to
+                # present the issue nicely, exit the script execution, and then
+                # post the error received from Active Directory
+
+                $vbm.Append("[GOPDKTP-SCCM] { PROCESS }: Error when ") > $null
+                $vbm.Append("creating OU $($vendor) at $($EnvPath) ") > $null
+                Write-Verbose -Message $vbm.ToString() -Verbose
+                $vbm.Clear() > $null
+
+                $vbm.Append("[GOPDKTP-SCCM] { PROCESS }: Since OU is ") > $null
+                $vbm.Append("not present, this script will terminate.") > $null
+                Write-Verbose -Message $vbm.ToString() -Verbose
+                $vbm.Clear() > $null
+
+                # Take the exception and throw it to stop script execution
+                throw $_
+            }
         } else {
             # Found the OU.
-            $vbm = "[GOPDKTP-SCCM] { PROCESS }: ActiveDirectory OU does"
-            $vbm += " exist for $($vendor) at "
-            $vbm += "$($oupath.Split(",",2)[-1])."
+            $vbm.Append("[GOPDKTP-SCCM] { PROCESS }: AD OU does exist") > $null
+            $vbm.Append("for $($vendor) at $($EnvPath)") > $null
             Write-Verbose -Message $vbm.ToString() -Verbose
             $vbm.Clear() > $null
-
-            $vbm = "[GOPDKTP-SCCM] { PROCESS }: Proceeding with creation "
-            $vbm += " of the Active Directory group in ."
-            Write-Verbose -Message $vbm.ToString() -Verbose
-            $vbm.Clear() > $null
-        }
-    }
-}
-<#
-        $ADParams = @{
-            Description = $desc
-            GroupCategory = "Security"
-            GroupScope = "Global"
-            Name = $grpName
-            SamAccountName = $grpName
-            Path = $oupath.Split(',',2)[-1]
-            PassThru = $true
-            ErrorAction = "Stop"
-            OutVariable = "adgrp"
         }
 
         if($Environment -eq "PROD") {
-                if($PreviousVersion) {
-                    if($ADPreviousVersionAction -eq "MigrateUsers") {
-                        $PrevDeploys = @{
-                            CimSession = Get-CimSession -Name SCCM
-                            Namespace = "root\sms\site_cm1"
-                            ClassName = "sms_deploymentsummary"
-                            Filter = "applicationname = $($PreviousVersion)"
+            if($PreviousVersion) {
+                if($ADPreviousVersionAction -eq "MigrateUsers") {
+                    $CimParams = @{
+                        Namespace = $ns
+                        ClassName = "sms_deploymentsummary"
+                        Filter = "applicationname = $($PreviousVersion)"
+                    }
+                    if(-not $OnPrimary) {
+                        $CimParams['CimSession'] = Get-CimSession -Name SCCM
+                    }
+                    $ds = Get-CimInstance @CimParams
+                    if($null -ne $ds) {
+                        $cols = foreach($d in $ds) {
+                            if(-not $OnPrimary) {
+                                $CimParams = @{
+                                    CimSession = $cs
+                                    Namespace = $ns
+                                    ClassName = "sms_collection"
+                                    Filter = "name = '$($d.collectionname)'"
+                                }
+                            } else {
+                                $CimParams = @{
+                                    Namespace = $ns
+                                    ClassName = "sms_collection"
+                                    Filter = "name = '$($d.collectionname)'"
+                                }
+                            }
+                            Get-CimInstance @CimParams
                         }
-                        $cols = Get-CimInstance @PrevDeploys
-                        if($cols.Count -gt 0) {
-                            $sb = New-Object -Type System.Text.StringBuilder
-                            $cols.where({
-                                $_.collectionname -notmatch "(NeilB)"
-                            }).foreach({
-                                $sb.Append("SUBSTRING(name,1,17) != '") > $null
-                                $sb.Append("'PWGSC-TPSGC-EM\\RT' AND ") > $null
-                                $sb.Append("collectionid = '") > $null
-                                $sb.Append("$($_.collectionid)'") > $null
-
-                                $fcmParams = @{
-                                    CimSession = Get-CimSession -Name SCCM
-                                    Namespace = "root\sms\site_cm1"
-                                    ClassName = "sms_fullcollectionmembership"
-                                    Filter = $sb.ToString()
-                                }
-                                $prevAdGrps = Get-CimInstance @fcmParams
-                                $members = $prevADGrps | ForEach-Object {
-                                    $Ldap = "(&(name=$($_.name.remove(0,16)))"
-                                    $Ldap += "(!(
-                                        member=*)))"
-                                    $PrevADParams = @{
-                                        LDAPFilter = $ldap
-                                        Properties = "member"
-                                    }
-                                    $m = Get-ADGroup @PrevADParams
-                                    if($m.member.Count -gt 0) {
-                                        $m.member
-                                    }
-                                }
+                        $rateregex = "(\ RT )|(\ RATE )|(^RT\ )"
+                        $colmem = ($cols | Get-CimInstance)
+                        $pgrp = foreach($c in $colmem) {
+                            $r = $c.collectionrules.where({
+                                $_.resourceclassname.endswith("usergroup") -and
+                                $_.rulename.Split("\")[-1] -notmatch $rateregex
                             })
+                            $prx = "\.(?=[^.]*$)"
+                            $r | Select-Object @{l="GrpName";e={
+                                $x = $_.rulename.split('\')[-1];
+                                $z = $x -split $prx
+                                if($z.count -ge 2) {
+                                    $s = "Added by"
+                                    $z.where({
+                                        -not $_.trim().startswith($s)
+                                    })
+                                } else {
+                                    $x
+                                }
+                            }}, @{l="CollectionName";e={$c.Name}}
+                        }
+                        $ADParams = @{
+                            LdapFilter = "(samAccountName=$($pgrp.grpname))"
+                            Properties = [string[]]("member", "info")
+                            OutVariable = "prevgrp"
+                        }
+                        Get-ADGroup @ADParams > $null
+                        if($null -eq $prevgrp) {
+                            $vbm.Append("[GOPDKTP-SCCM] { PROCESS }: ") > $null
+                            $vbm.Append("The AD group $($pgrp.grpname)") > $null
+                            $vbm.Append(" does not exist.") > $null
+                            Write-Verbose -Message $vbm.ToString() -Verbose
+                            $vbm.Clear()
+                            $vbm.Append("[GOPDKTP-SCCM] { PROCESS }: ") > $null
+                            $vbm.Append("Group Membership cannot be ") > $null
+                            $vbm.Append("migrated at this time.") > $null
+                            Write-Verbose -Message $vbm.ToString() -Verbose
+                            $vbm.Clear()
+                        } else {
+                            if($null -eq $prevgrp.member) {
+                                $vbm.Append("[GOPDKTP-SCCM] { PROCESS") > $null
+                                $vbm.Append("}: Group $($prevgrp.Name)") > $null
+                                $vbm.Append(" does exist in AD but the") > $null
+                                $vbm.Append(" membership is empty") > $null
+                                Write-Verbose -Message $vbm.ToString() -Verbose
+                                $vbm.Clear()
+                                $vbm.Append("[GOPDKTP-SCCM] { PROCESS") > $null
+                                $vbm.Append("}: Group Members cannot") > $null
+                                $vbm.Append(" be migrated.") > $null
+                                Write-Verbose -Message $vbm.ToString() -Verbose
+                                $vbm.Clear()
+                            } else {
+                                $vbm.Append("[GOPDKTP-SCCM] { PROCESS") > $null
+                                $vbm.Append(" }: Group $($prevgrp.Name)") > $null
+                                $vbm.Append(" does exist in AD and the") > $null
+                                $vbm.Append(" membership is not empty") > $null
+                                Write-Verbose -Message $vbm.ToString() -Verbose
+                                $vbm.Clear()
+                                $vbm.Append("[GOPDKTP-SCCM] { PROCESS") > $null
+                                $vbm.Append(" }: Group Members will") > $null
+                                $vbm.Append(" be migrated to new group") > $null
+                                Write-Verbose -Message $vbm.ToString() -Verbose
+                                $vbm.Clear()
+                            }
                         }
                     }
                 }
             }
-            if($Environment -eq "RATE") {
-                $ADParams['OtherAttributes'] = @{ info = $info.ToString() }
-            }
+        }
+
+        if($Environment -eq "RATE") {
+            $ADParams['OtherAttributes'] = @{ info = $info.ToString() }
+        }
 
                 try {
 
@@ -680,10 +786,10 @@ Function New-GOPDKTPApplicationDeployment {
                         Write-Verbose -Message "[GOPDKTP-SCCM] { PROCESS }: Issue creating AD group $($grpName) at $($p)" -Verbose
                         if(-not $PathExists) {
                             Write-Verbose -Message "[GOPDKTP-SCCM] { PROCESS }: Path $($p) does not exist in Active Directory" -Verbose
-                            $ouname, $oupath = $p.Split(",",2)
+                            $ouname, $oul = $p.Split(",",2)
                             $ouname = $ouname.replace("OU=","")
                             $NewOUParams = @{
-                                Path = $oupath
+                                Path = $oul
                                 Name = $ouname
                                 ProtectedFromAccidentalDeletion = $false
                             }
